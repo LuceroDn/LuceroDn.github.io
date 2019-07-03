08b0829a1acf77372581268c0e133cc709ceafb1
Lucero Dn <lucero_dn@hotmail.es>
2019-07-02 20:32:20 -0500

    Se agregaron componentes de Presentation y Navbar

---
+++ portafolio/src/Components/NavBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+   
+  render() {
+    return (
+     <header>
+        <nav>
+    <div className="nav-wrapper">
+      <a href="cvv" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+      <ul className="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul className="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+  </ul>
+
+     </header>
+    );
+  }
+}
+
+
+export default NavBar;
---
+++ portafolio/src/Components/Presentation.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Box from "./Box";
+
+
+class Presentation extends Component {
+    
+  render() {
+    return (
+        <div className="background1">
+            <NavBar />
+            <Box />
+        </div>
+        
+    );
+  }
+}
+
+
+export default Presentation;
