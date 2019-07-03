833483550e764a04638058a5c49864bf7b4bec59
Lucero Dn <lucero_dn@hotmail.es>
2019-07-02 21:21:36 -0500

    Updates

---
+++ precache-manifest.782323766149b4df348f2a1e47acdcd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a97e4b767f458444af0f18ed6c1fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e3f690ee0160895625",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "0cc4a64b8a891fa09cd3",
+    "url": "/static/css/main.726029e8.chunk.css"
+  },
+  {
+    "revision": "c7e3f690ee0160895625",
+    "url": "/static/js/2.4adcb376.chunk.js"
+  },
+  {
+    "revision": "0cc4a64b8a891fa09cd3",
+    "url": "/static/js/main.7f05247a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "30b625880e958ed30bf519b3fbcbff38",
+    "url": "/static/media/fondo.30b62588.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.782323766149b4df348f2a1e47acdcd4.js"
 );
 
 self.addEventListener('message', (event) => {
