4a4b9c8ff0c7d8b5e969c826867cbb69037d90ef
Lucero Dn <lucero_dn@hotmail.es>
2019-07-02 20:12:15 -0500

    Updates

---
+++ precache-manifest.e6c370eb9a065ee59ea2094f9a9d4f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4736365707f1fd47a09d8a579d7373",
+    "url": "/LuceroDn.github.io-Portafolio/index.html"
+  },
+  {
+    "revision": "62a83b46c993e308cb4f",
+    "url": "/LuceroDn.github.io-Portafolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/LuceroDn.github.io-Portafolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "62a83b46c993e308cb4f",
+    "url": "/LuceroDn.github.io-Portafolio/static/js/main.4bcffffc.chunk.js"
+  },
+  {
+    "revision": "eaee11c6c6b69feaa9a1",
+    "url": "/LuceroDn.github.io-Portafolio/static/js/runtime~main.c2060f07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LuceroDn.github.io-Portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
