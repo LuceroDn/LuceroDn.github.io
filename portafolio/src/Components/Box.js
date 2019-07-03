a7d502427ce01f8811152505169a436c611512b8
Lucero Dn <lucero_dn@hotmail.es>
2019-07-02 21:24:54 -0500

    Se creo componente 'Box'

---
+++ portafolio/src/Components/Box.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+//import logo from "../img/logo.png";
+
+
+class Box extends Component {
+  render() {
+    return (
+   <div className="box">
+    <div>
+        <h1 className="name">Lucero Dn</h1>
+    </div>
+    <div>
+        <p className="FrontE">Front-End Developer</p>
+    </div>
+   </div>
+
+    );
+  }
+}
+
+
+export default Box;
