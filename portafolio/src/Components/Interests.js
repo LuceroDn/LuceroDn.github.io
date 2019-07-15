d1c0bfd33a0f939ed313170f87b865c79fd226e4
Lucero Dn <lucero_dn@hotmail.es>
2019-07-15 09:48:49 -0500

    Se agregaron imagenes a Interests y se aplico estilo igual a los titulos de cada componente

--- portafolio/src/Components/Interests.js
+++ portafolio/src/Components/Interests.js
@@ -1,10 +1,37 @@
-import Reac, {Component} from 'react';
+import React, {Component} from 'react';
+import animals from "../img/animales.png";
+import airplane from "../img/avion.png";
+import music from "../img/musica.png";
+import ux from "../img/ux.png";
+import planet from "../img/planeta.png";
 
 class Interests extends Component {
     render(){
         return(
-            <div>
+            <div className="container-fluid backgroundInterests">
+                <div className="row">
+                        <h1 className="titlePages">INTERESES</h1>
+                    <div className="col">
 
+              <div className="col s6 interest">
+                <img className="" src={animals} alt="animals" />
+              </div>
+              <div className="col s6 interest">
+                <img className="" src={airplane} alt="airplane" />
+              </div>
+              <div className="col s12 interest2">
+                <img className="" src={music} alt="music" />
+              </div>
+
+              <div className="col s6 interest">
+                <img className="" src={ux} alt="ux" />
+              </div>
+              <div className="col s6 interest">
+                <img className="" src={planet} alt="planet" />
+              </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
--- portafolio/src/Components/Skills.js
+++ portafolio/src/Components/Skills.js
@@ -2,53 +2,46 @@ import React, { Component } from 'react';
 import imagen from '../img/code.jpg'
 
 class Skills extends Component {
-    render(){
-        return(
-            <div className="container-fluid">
-                <div className="row skills">
-                    <div>
-                    <div>
-            <h2 className="aboutMe">Habilidades</h2>
-            </div>
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row skills">
+          <div className="">
 
-      <h3 className="">Técnicas</h3>
-      
+            <h2 className="titlePages">Habilidades</h2>
+
+
+            <h3 className="">Técnicas</h3>
+
             <div className="col">
 
-<div class="col s6">
-  <img className="imgSkills" src={imagen} alt="imgSkills"/>
-</div>
-<div class="col s6">
-<img className="imgSkills" src={imagen} alt="imgSkills"/>
-</div>
-<div class="col s6">
-<img className="imgSkills" src={imagen} alt="imgSkills"/>
-</div>
+              <div class="col s6">
+                <img className="imgSkills" src={imagen} alt="imgSkills" />
+              </div>
+              <div class="col s6">
+                <img className="imgSkills" src={imagen} alt="imgSkills" />
+              </div>
+              <div class="col s6">
+                <img className="imgSkills" src={imagen} alt="imgSkills" />
+              </div>
 
-<div class="col s6">
-  <img className="imgSkills" src={imagen} alt="imgSkills"/>
-</div>
-<div class="col s6">
-<img className="imgSkills" src={imagen} alt="imgSkills"/>
-</div>
-<div class="col s6">
-<img className="imgSkills" src={imagen} alt="imgSkills"/>
-</div>
+              <div class="col s6">
+                <img className="imgSkills" src={imagen} alt="imgSkills" />
+              </div>
+              <div class="col s6">
+                <img className="imgSkills" src={imagen} alt="imgSkills" />
+              </div>
+              <div class="col s6">
+                <img className="imgSkills" src={imagen} alt="imgSkills" />
+              </div>
 
-</div>
-
-
-
-
-
-
-      
-
-                    </div>
-                </div>
             </div>
-        )
-    }
+
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Skills;
