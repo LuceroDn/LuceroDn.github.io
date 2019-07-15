61aca50aa920f9408d9645fd1f80ad88a159c3d8
Lucero Dn <lucero_dn@hotmail.es>
2019-07-15 13:58:02 -0500

    Se agrego contenido al componente 'Contact' e imagenes

--- portafolio/src/Components/Contact.js
+++ portafolio/src/Components/Contact.js
@@ -1,10 +1,23 @@
 import React, {Component} from "react";
+import WhatsApp from "../img/whatsapp.png";
+import Github from "../img/git.png";
+import LinkedIn from "../img/linkedin.png";
+import EMail from "../img/mail.png"
+
+
 
 class Contact extends Component {
     render(){
         return(
             <div>
+                <h1 className="titlePages">CONTACTO</h1>
 
+                <p>¿Te gustaría que trabajemos juntos? ¿Tienes alguna duda o comentario? Escribeme, con gusto te responderé.</p>
+
+                <a href="#"><img className="imgContact" src={WhatsApp} alt="whatsapp" /></a>
+                <a href="#"><img className="imgContact" src={EMail} alt="mail" /></a>
+                <a href="#"><img className="imgContact" src={LinkedIn} alt="linkedin" /></a>
+                <a href="#"><img className="imgContact" src={Github} alt="github" /></a>
             </div>
         )
     }
