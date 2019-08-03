824448eb531c73eee5bb84363cabcec794f7cd25
Lucero Dn <lucero_dn@hotmail.es>
2019-08-03 02:22:52 -0500

    Se hizo responsive el componente AboutMe

--- portafolio/src/Components/AboutMe.js
+++ portafolio/src/Components/AboutMe.js
@@ -8,18 +8,21 @@ class AboutMe extends Component {
   render() {
     return (
       <div className="container-fluid backgroundAbout">
-        <div className="row center-xs">
+        <div className="row center">
           <div className="col">
 
-            <div className="aboutBox center-xs">
+            <div className="aboutBox">
             <div>
-              <h2 className="titlePages titleAbout">ACERCA DE MI</h2>
+              <h2 className="titlePages titleAbout col s6 m6 l6 xl6 offset-s3 offset-m3 offset-l3 offset-xl4">ACERCA DE MI</h2>
             </div>
-            <p className="textAbout">Hola, soy Lucero, soy desarrolladora web y administradora de empresas. Me gusta mucho viajar, leer y los animales.</p>
-            <p className="textAbout textAbout2">Aquí podrás encontrar algunos proyectos en los que he trabajado en Laboratoria</p>
 
-            <div className="row center-xs">
-              <div className="col">
+            <div className="row center">
+            <p className="textAbout col s10 m12 l12 xl12 offset-s1 offset-xl1">Hola, soy Lucero, soy desarrolladora web y administradora de empresas. Me gusta mucho viajar, leer y los animales.</p>
+            <p className="textAbout textAbout2 col s10 m10 xl12  offset-s1 offset-m1 offset-xl1">Aquí podrás encontrar algunos proyectos en los que he trabajado en Laboratoria</p>
+            </div>
+
+            <div className="row">
+              <div className="col s8 xl 10 offset-s2 offset-xl3">
                 <img className="imgTravel" src={travel} alt="travel" />
               </div>
             </div>
